e2327c780893c5069201dfaa16a9bedd330d99ba
devAnderson <chltjdrhd777@naver.com>
2023-07-21 16:26:00 +0900

    [Style] 스타일링 오류 수정
    1. 현재 다크모드로 변경 시, 공용에서 지정한 h5의 미디어쿼리가 자식에 적용한 미디어쿼리보다 우선하는 현상을 발견했습니다. 해결 방법을 여러모로 시도한 결과 우선 !important로 자식 디자인을 우선적용하도록 수정하였고, 추후 수정 예정입니다.
    
    2. h5의 좌측 간격이 가독성을 해치는 문제가 있어서 위치를 조정하였습니다
    
    3. 스크롤의 바운싱 액션으로 인해 백그라운드의 하얀 부분이 노출되는 문제가 있어, useTheme을 활용해 글로벌 스타일링도 theme 테마의 백그라운드를 바라보도록 수정하였습니다.

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,75 +1,78 @@
-import { Global, css } from '@emotion/react';
-
-const globalCSS = css`
-  :root {
-    --zIndex-1st: 10000;
-    --zIndex-2st: 9000;
-    --zIndex-3st: 8000;
-  }
-
-  @font-face {
-    font-family: 'NotoSansKR';
-    src: url('/fonts/NotoSans-Regular.woff') format('woff');
-    font-style: normal;
-    font-weight: normal;
-    font-display: '';
-  }
-
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    word-break: keep-all;
-  }
-  html {
-    font-size: 62.5%;
-    scroll-behavior: smooth;
-    overflow-x: hidden;
-  }
-  body {
-    font-size: 1.6rem;
-    font-family: 'NotoSansKR', sans-serif;
-    user-select: none;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    -o-user-select: none;
-  }
-  a {
-    text-decoration: none;
-    color: black;
-  }
-  ul {
-    list-style-type: none;
-  }
-  img {
-    -webkit-touch-callout: none;
-    -webkit-user-select: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-  }
-  button {
-    border: none;
-    background-color: transparent;
-    border-radius: 1rem;
-    cursor: pointer;
-    &:active,
-    &:focus {
-      outline: none;
-    }
-  }
-  input {
-    outline: none;
-    &:focus::placeholder {
-      color: transparent;
-    }
-  }
-`;
+import { Global, Theme, css, useTheme } from '@emotion/react';
 
 function GlobalStyle() {
-  return <Global styles={globalCSS} />;
+  const theme = useTheme();
+
+  const globalCSS = (theme: Theme) => css`
+    :root {
+      --zIndex-1st: 10000;
+      --zIndex-2st: 9000;
+      --zIndex-3st: 8000;
+    }
+
+    @font-face {
+      font-family: 'NotoSansKR';
+      src: url('/fonts/NotoSans-Regular.woff') format('woff');
+      font-style: normal;
+      font-weight: normal;
+      font-display: '';
+    }
+
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      word-break: keep-all;
+    }
+    html {
+      font-size: 62.5%;
+      scroll-behavior: smooth;
+      overflow-x: hidden;
+      background-color: ${theme.backgroundColor};
+    }
+    body {
+      font-size: 1.6rem;
+      font-family: 'NotoSansKR', sans-serif;
+      user-select: none;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      -o-user-select: none;
+    }
+    a {
+      text-decoration: none;
+      color: black;
+    }
+    ul {
+      list-style-type: none;
+    }
+    img {
+      -webkit-touch-callout: none;
+      -webkit-user-select: none;
+      -khtml-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+    }
+    button {
+      border: none;
+      background-color: transparent;
+      border-radius: 1rem;
+      cursor: pointer;
+      &:active,
+      &:focus {
+        outline: none;
+      }
+    }
+    input {
+      outline: none;
+      &:focus::placeholder {
+        color: transparent;
+      }
+    }
+  `;
+
+  return <Global styles={globalCSS(theme)} />;
 }
 
 export default GlobalStyle;
