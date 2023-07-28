76267aa2873421e05ab518af4f8f831fee192870
devAnderson <chltjdrhd777@naver.com>
2023-07-28 18:38:14 +0900

    [Fix] 컬러피커 모바일 터치 핸들러 추가
    모바일 부분은 마우스클릭과 다른 메커니즘으로 움직여야 하기 때문에 핸들러들을 분리하여 적절하게 로직을 추가 및 보완하였습니다.

--- src/components/CanvasMenu.tsx
+++ src/components/CanvasMenu.tsx
@@ -3,28 +3,37 @@ import MenuBtn from './Button/MenuBtn';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 import ColorPicker from './ColorPicker';
+import { useRecoilValue } from 'recoil';
+import { memoCanvasAtom } from 'recoil/memo';
 
 function CanvasMenu() {
+  const { isCanvasOpen } = useRecoilValue(memoCanvasAtom);
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <Container>
+    <Container isCanvasOpen={isCanvasOpen}>
       <MenuBtn className={menuOpen && 'active'} onClick={() => setMenuOpen((prev) => !prev)} />
 
-      <MenuBox>
-        <ColorPicker />
-      </MenuBox>
+      {menuOpen && (
+        <MenuBox>
+          <ColorPicker />
+        </MenuBox>
+      )}
     </Container>
   );
 }
 
-const Container = styled.div`
+const Container = styled.div<{ isCanvasOpen: boolean }>`
   position: fixed;
   top: calc(var(--header-height) + 1rem);
   right: var(--option-btn-right);
   display: flex;
   flex-direction: column;
   align-items: flex-end;
+
+  transition: opacity 0.1s ease-in, visibility 0.1s ease-in;
+  opacity: ${({ isCanvasOpen }) => (isCanvasOpen ? 1 : 0)};
+  visibility: ${({ isCanvasOpen }) => (isCanvasOpen ? 'visible' : 'hidden')};
 `;
 
 const MenuBox = styled.div``;
